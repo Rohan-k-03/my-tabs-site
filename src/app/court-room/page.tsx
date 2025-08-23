405e2638cc276b44fd902500f0c0c3c61cc1aeb0
Rohan-k-03 <21358295@students.latrobe.edu.au>
2025-08-23 19:13:33 +1000

    Update page.tsx

--- src/app/court-room/page.tsx
+++ src/app/court-room/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion, Variants } from "framer-motion";
 
 const courtTasks = [
-"Comming soon..."
+  "Coming soon...",
 ];
 
 const containerVariants: Variants = {
